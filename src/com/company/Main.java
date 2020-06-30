228a261490f2a35b9b9846786b6be6b76f614a53
anykey-vrn <my4u@bk.ru>
2020-06-30 20:47:54 +0300

    Third commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println ("Helo World");
+        System.out.println ("Second commit");
+        System.out.println ("Next commit");
+    }
+}
